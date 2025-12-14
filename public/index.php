f437f0a0a985df4e91e8dd3e58f8f420d960661e
murYG <mur@jinr.ru>
2025-12-14 15:17:29 +0300

    step 1, c4

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
-111
 <?php
 
-/*
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
@@ -14,11 +12,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
-*/
+
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-/*
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
-*/
