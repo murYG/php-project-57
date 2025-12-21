483394ceca8025f2b2d061d58bc483eea5c4497e
murYG <mur@jinr.ru>
2025-12-21 21:20:31 +0300

    step 6

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,24 +2,21 @@
     <h2 class="text-center"><a href="{{ route('welcome') }}">Менеджер задач</a></h2>
     
     <!-- Validation Errors -->
-    <x-input-error-auth :messages="$errors->get('name')" class="mt-2" />
-    <x-input-error-auth :messages="$errors->get('email')" class="mt-2" />
-    <x-input-error-auth :messages="$errors->get('password')" class="mt-2" />
-    <x-input-error-auth :messages="$errors->get('password_confirmation')" class="mt-2" />
 
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Имя')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error-auth :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error-auth :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -30,6 +27,7 @@
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
+            <x-input-error-auth :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
@@ -39,6 +37,7 @@
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
+            <x-input-error-auth :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
@@ -46,9 +45,9 @@
                 {{ __('Уже зарегистрированы?') }}
             </a>
 
-            <x-primary-button class="ms-4" name="Зарегистрировать">
+            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
                 Зарегистрировать
-            </x-primary-button>
+            </button>
         </div>
     </form>
 </x-guest-layout>
