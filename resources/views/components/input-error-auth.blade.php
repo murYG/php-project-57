2f586e1e5c3a0effa357e09fe2bd3df41ae27be4
murYG <mur@jinr.ru>
2025-12-21 21:51:32 +0300

    step 6

--- resources/views/components/input-error-auth.blade.php
+++ resources/views/components/input-error-auth.blade.php
@@ -2,10 +2,8 @@
 
 @if ($messages)
     <div class="mb-4">
-        <div {{ $attributes->merge(['class' => 'font-medium text-red-600']) }}>
-                Упс! Что-то пошло не так: 
-        </div>
-        <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+        <div class="font-medium text-red-600">{{ __('Упс! Что-то пошло не так:') }}</div>
+        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
             @foreach ((array) $messages as $message)
                 <li>{{ $message }}</li>
             @endforeach
