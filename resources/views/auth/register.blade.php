a1c81e35f78bbc77beab60e318f3eb17b8158247
murYG <mur@jinr.ru>
2025-12-21 21:06:42 +0300

    step 6

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,11 @@
 <x-guest-layout>
     <h2 class="text-center"><a href="{{ route('welcome') }}">Менеджер задач</a></h2>
+    
+    <!-- Validation Errors -->
+    <x-input-error-auth :messages="$errors->get('name')" class="mt-2" />
+    <x-input-error-auth :messages="$errors->get('email')" class="mt-2" />
+    <x-input-error-auth :messages="$errors->get('password')" class="mt-2" />
+    <x-input-error-auth :messages="$errors->get('password_confirmation')" class="mt-2" />
 
     <form method="POST" action="{{ route('register') }}">
         @csrf
@@ -8,14 +14,12 @@
         <div>
             <x-input-label for="name" :value="__('Имя')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error-auth :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error-auth :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -26,8 +30,6 @@
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
-
-            <x-input-error-auth :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
@@ -37,8 +39,6 @@
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error-auth :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
