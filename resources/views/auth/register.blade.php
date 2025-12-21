03035b05c61b15f003c43dac7df15166b92b874b
murYG <mur@jinr.ru>
2025-12-21 21:27:28 +0300

    step 6

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,7 +45,7 @@
                 {{ __('Уже зарегистрированы?') }}
             </a>
 
-            <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+            <button id="register" type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
                 Зарегистрировать
             </button>
         </div>
