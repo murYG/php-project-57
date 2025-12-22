70515e861f6f7930261375bbed65a187c888f88c
murYG <mur@jinr.ru>
2025-12-22 23:01:36 +0300

    step 6

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,8 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    
+    public function testThatTrueIsTrue(): void
+    {
+    }
 }
