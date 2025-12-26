5cd690f0915cb173ba235194d71a826324880b23
murYG <mur@jinr.ru>
2025-12-26 22:46:50 +0300

    step 6

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
- * @method static \Illuminate\Database\Eloquent\Builder|static labels()
+ * @method static \Illuminate\Database\Eloquent\Model|static labels()
  */
 class Task extends Model
 {
